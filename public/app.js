bf29f1dc2e9a5a359967959a7754bf3a41bf2513
Dan Oh <danoh82@gmail.com>
2018-02-06 18:37:31 -0800

    'eploying to heroku

--- public/app.js
+++ public/app.js
@@ -1,44 +1,37 @@
 
 
 // Grab the articles as a json
-$.getJSON("/articles", function(data) {
-   console.log("GOT JSON ARTICLES")
-    // For each one
-    for (var i = 0; i < data.length; i++) {
-      // Display the apropos information on the page
-      $("#articleSpace").append("<h2><a href='" + data[i].link + "'>" + data[i].title + "</a></h2>");
-      // A button to submit a new note, with the id of the article saved to it
-      $("#articleSpace").append("<button data-id='" + data[i]._id + "' id='savenote'>Save Article</button>");
-      // $("#articleSpace").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-      // "<div class='panel panel-default'>"
-      // "<div class='panel panel-heading'>"
-      // "<h3>" "<a class='article-link' href='" + data[i].link +' ">" ' + data[i].title +' "</a>" "</h3>"
-      // "</div>"
-      // "</div>"
-      // "<div class='panel-body'>"
+$.getJSON("/articles", function (data) {
+  console.log(data);
+  console.log("GOT JSON ARTICLES");
+  // For each one
+  for (var i = 0; i < data.length; i++) {
+    // Display the apropos information on the page
+    $("#articleSpace").append("<h2><a href='" + data[i].link + "'>" + data[i].title + "</a></h2>");
+    // A button to submit a new note, with the id of the article saved to it
+    $("#articleSpace").append("<button class='btn btn-danger save' data-id='" + data[i]._id + "' id='savenote'>Save Article</button><hr>");
 
-      // "</div>"
-    }
-  });
-  
-  
-  // Whenever someone clicks Scrap new articles button
-  $(".btn-danger").on("click", function() {
-    $("#articleSpace").empty();
-    console.log("SCRAPE NEW ARTICLES")
-    // Empty the notes from the note section
-    $.get("/scrape", function(data){
-      $.getJSON("/articles", function(data){
-        for (var i = 0; i < data.length; i++) {
-          // Display the apropos information on the page
-          $("#articleSpace").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-        };
-      });
+  }
+});
+
+
+// Whenever someone clicks Scrap new articles button
+$(".btn-danger").on("click", function () {
+  $("#articleSpace").empty();
+  console.log("SCRAPE NEW ARTICLES")
+  // Empty the notes from the note section
+  $.get("/scrape", function (data) {
+    $.getJSON("/articles", function (data) {
+      for (var i = 0; i < data.length; i++) {
+        // Display the apropos information on the page
+        $("#articleSpace").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+      };
     });
   });
+});
 
-  // Whenever someone clicks a p tag
-$(document).on("click", "p", function() {
+// Whenever someone clicks a p tag
+$(document).on("click", "p", function () {
   // Empty the notes from the note section
   $("#notes").empty();
   // Save the id from the p tag
@@ -50,7 +43,7 @@ $(document).on("click", "p", function() {
     url: "/articles/" + thisId
   })
     // With that done, add the note information to the page
-    .done(function(data) {
+    .done(function (data) {
       console.log(data);
       // The title of the article
       $("#notes").append("<h2>" + data.title + "</h2>");
@@ -70,33 +63,32 @@ $(document).on("click", "p", function() {
       }
     });
 });
-  
-  // When you click the savenote button
-  $(document).on("click", "#savenote", function() {
-    // Grab the id associated with the article from the submit button
-    var thisId = $(this).attr("data-id");
-  
-    // Run a POST request to change the note, using what's entered in the inputs
-    $.ajax({
-      method: "POST",
-      url: "/articles/" + thisId,
-      data: {
-        // Value taken from title input
-        title: $("#titleinput").val(),
-        // Value taken from note textarea
-        body: $("#bodyinput").val()
-      }
-    })
-      // With that done
-      .done(function(data) {
-        // Log the response
-        console.log(data);
-        // Empty the notes section
-        $("#notes").empty();
-      });
-  
-    // Also, remove the values entered in the input and textarea for note entry
-    $("#titleinput").val("");
-    $("#bodyinput").val("");
-  });
-  
+
+// When you click the savenote button
+$(document).on("click", "#savenote", function () {
+  // Grab the id associated with the article from the submit button
+  var thisId = $(this).attr("data-id");
+
+  // Run a POST request to change the note, using what's entered in the inputs
+  $.ajax({
+    method: "POST",
+    url: "/articles/" + thisId,
+    data: {
+      // Value taken from title input
+      title: $("#titleinput").val(),
+      // Value taken from note textarea
+      body: $("#bodyinput").val()
+    }
+  })
+    // With that done
+    .done(function (data) {
+      // Log the response
+      console.log(data);
+      // Empty the notes section
+      $("#notes").empty();
+    });
+
+  // Also, remove the values entered in the input and textarea for note entry
+  $("#titleinput").val("");
+  $("#bodyinput").val("");
+});
